8cbab2d4b2bc4724f7a38f09c174ae2e132a3e20
Duc Ho <hotienduc05012002@gmail.com>
2022-10-31 06:18:30 -0600

    Added the Storage class and Comparators for Ingredient

---
+++ app/src/main/java/com/cmput301f22t18/snackntrack/IngredientCategoryComparator.java
@@ -0,0 +1,11 @@
+package com.cmput301f22t18.snackntrack;
+
+/**
+ * This comparator compares two Ingredients based on their Category
+ */
+public class IngredientCategoryComparator extends IngredientComparator {
+    @Override
+    public int compare(Ingredient ingredient1, Ingredient ingredient2) {
+        return ingredient1.getCategory().compareTo(ingredient2.getCategory());
+    }
+}
---
+++ app/src/main/java/com/cmput301f22t18/snackntrack/IngredientComparator.java
@@ -0,0 +1,11 @@
+package com.cmput301f22t18.snackntrack;
+
+import java.util.Comparator;
+
+/**
+ * This abstract class represents a comparator for Ingredient objects
+ */
+public abstract class IngredientComparator implements  Comparator<Ingredient>{
+    public abstract int compare(Ingredient ingredient1, Ingredient ingredient2);
+}
+
