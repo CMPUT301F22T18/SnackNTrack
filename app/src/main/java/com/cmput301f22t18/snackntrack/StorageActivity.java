c6435771fba70ae682b56f43c6dfc4e0bacb621e
Duc Ho <hotienduc05012002@gmail.com>
2022-10-31 11:12:51 -0600

    Added Add Ingredient Fragment, Linked Storage Activity to Add Ingredient Fragment, Finished Back Button

--- app/src/main/java/com/cmput301f22t18/snackntrack/StorageActivity.java
+++ app/src/main/java/com/cmput301f22t18/snackntrack/StorageActivity.java
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.Date;
 
 public class StorageActivity extends AppCompatActivity {
@@ -26,6 +28,20 @@ public class StorageActivity extends AppCompatActivity {
         recyclerView.setAdapter(storageAdapter);
         recyclerView.setLayoutManager(
                 new LinearLayoutManager(getApplicationContext()));
+
+        FloatingActionButton fab = findViewById(R.id.add_ingredient_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (savedInstanceState == null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .setReorderingAllowed(true)
+                            .add(R.id.fragment_container_view, AddIngredientFragment.class, null)
+                            .addToBackStack("AddIngredient")
+                            .commit();
+                }
+            }
+        });
     }
 
     private void initStorage() {
