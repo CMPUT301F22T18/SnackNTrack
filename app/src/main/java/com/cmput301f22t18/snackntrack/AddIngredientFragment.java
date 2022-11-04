a69576d4a5e03d66eb1c0a2535c8cfe9445f5bce
Duc Ho <hotienduc05012002@gmail.com>
2022-11-03 21:13:55 -0600

    Added Date Picker and Cancel Button

--- app/src/main/java/com/cmput301f22t18/snackntrack/AddIngredientFragment.java
+++ app/src/main/java/com/cmput301f22t18/snackntrack/AddIngredientFragment.java
@@ -4,15 +4,21 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Objects;
 
 public class AddIngredientFragment extends Fragment {
 
@@ -26,6 +32,9 @@ public class AddIngredientFragment extends Fragment {
     // private String mParam2;
 
     FloatingActionButton floatingActionButton;
+    Spinner unit, location, category;
+    ImageButton pickBestBeforeButton;
+    Button cancel, add;
 
     public AddIngredientFragment() {
         // Required empty public constructor
@@ -68,14 +77,49 @@ public class AddIngredientFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         ImageButton imageButton = view.findViewById(R.id.back_button);
         floatingActionButton = getActivity().findViewById(R.id.add_ingredient_fab);
-        imageButton.setOnClickListener(v -> {
-            Log.i("Debug", "Pressed Back!");
-            floatingActionButton.show();
-            getActivity().getSupportFragmentManager().popBackStackImmediate();
+        location = view.findViewById(R.id.location_spinner);
+        unit = view.findViewById(R.id.unit_spinner);
+        category = view.findViewById(R.id.category_spinner);
+
+        ArrayAdapter<CharSequence> unit_adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.units_array, android.R.layout.simple_spinner_item);
+
+        ArrayAdapter<CharSequence> location_adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.ingredient_locations_array, android.R.layout.simple_spinner_item);
+
+        ArrayAdapter<CharSequence> category_adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.ingredient_categories_array, android.R.layout.simple_spinner_item);
+
+        unit_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        location_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        category_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        unit.setAdapter(unit_adapter);
+        location.setAdapter(location_adapter);
+        category.setAdapter(category_adapter);
+
+        pickBestBeforeButton = view.findViewById(R.id.date_picker_button);
+
+        pickBestBeforeButton.setOnClickListener(v -> {
+            DialogFragment newFragment = new DatePickerFragment();
+            newFragment.show(getChildFragmentManager(), "datePicker");
+
         });
+
+        cancel = view.findViewById(R.id.cancel_add_ingredient_button);
+
+        imageButton.setOnClickListener(v -> goBack());
+
+        cancel.setOnClickListener(v -> goBack());
 
         // Set FAB to hide
 
         floatingActionButton.hide();
+    }
+
+    public void goBack() {
+        Log.i("Debug", "Pressed Back!");
+        floatingActionButton.show();
+        requireActivity().getSupportFragmentManager().popBackStackImmediate();
     }
 }
---
+++ app/src/main/java/com/cmput301f22t18/snackntrack/DatePickerFragment.java
@@ -0,0 +1,45 @@
+package com.cmput301f22t18.snackntrack;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class DatePickerFragment extends DialogFragment
+        implements DatePickerDialog.OnDateSetListener {
+
+    @Override
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(requireContext(), this, year, month, day);
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+
+        TextView calendarText = getParentFragment().getView().findViewById(R.id.best_before_date);
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy", Locale.CANADA);
+        calendarText.setText(df.format(calendar.getTime()));
+
+
+    }
+}
