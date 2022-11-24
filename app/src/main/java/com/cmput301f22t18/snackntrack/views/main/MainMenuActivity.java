665514659101f7dda433462e8c3bd58f8cd32f8d
Duc Ho <hotienduc05012002@gmail.com>
2022-11-24 15:52:53 -0700

    Added database integration

--- app/src/main/java/com/cmput301f22t18/snackntrack/views/main/MainMenuActivity.java
+++ app/src/main/java/com/cmput301f22t18/snackntrack/views/main/MainMenuActivity.java
@@ -17,6 +17,8 @@ import com.cmput301f22t18.snackntrack.models.Recipe;
 import com.cmput301f22t18.snackntrack.models.RecipeList;
 import com.cmput301f22t18.snackntrack.views.storage.StorageActivity;
 import com.cmput301f22t18.snackntrack.views.storage.StorageFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -25,6 +27,9 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -41,38 +46,60 @@ public class MainMenuActivity extends AppCompatActivity {
         storageFragment = new StorageFragment();
         RecipeList rl = new RecipeList();
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        assert user != null;
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-//        CollectionReference cr = db.collection("recipeLists")
-//                .document(user.getUid()).collection("recipes");
-//        cr.addSnapshotListener((value, error) -> {
-//            if (error != null) {
-//                Log.w(TAG, "Listen failed.", error);
-//                return;
-//            }
-//            rl.getRecipeList().clear();
-//            for (QueryDocumentSnapshot doc : value) {
-//                CollectionReference cr2 = cr.document(doc.getId()).collection("ingredients");
-//                Recipe r = doc.toObject(Recipe.class);
-//                for (DocumentSnapshot doc2 : cr2.get().getResult()) {
-//                    Log.d(TAG, doc2.toObject(Ingredient.class).toString());
-//                }
-//                rl.addRecipe(doc.toObject(Recipe.class));
-//
-//            }
-//        });
+        CollectionReference cr = db.collection("recipeLists")
+                .document(user.getUid()).collection("recipes");
+        cr.addSnapshotListener((value, error) -> {
+            if (error != null) {
+                Log.w(TAG, "Listen failed.", error);
+                return;
+            }
+            rl.getRecipeList().clear();
+            for (QueryDocumentSnapshot doc : value) {
+                Recipe r = doc.toObject(Recipe.class);
+                r.setRecipeIngredients(new ArrayList<>());
+                CollectionReference cr2 = cr
+                        .document(doc.getId())
+                        .collection("ingredients");
+                cr2.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                if (document != null)
+                                    r.addIngredient(document.toObject(Ingredient.class));
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        }
+                    }
+                });
+                rl.addRecipe(r);
+            }
+        });
+        Log.d(TAG, rl.toString());
         recipeListFragment = RecipeListFragment.newInstance(rl);
 
         bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+        final int id_storage = R.id.storage;
+        final int id_recipes = R.id.recipes;
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
-                    case R.id.storage:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_main, storageFragment).commit();
+                    case id_storage:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fragment_container_main, storageFragment)
+                                .commit();
                         return true;
 
-                    case R.id.recipes:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_main, recipeListFragment).commit();
+                    case id_recipes:
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fragment_container_main,
+                                        RecipeListFragment.newInstance(rl))
+                                .commit();
                         return true;
                 }
                 return false;
