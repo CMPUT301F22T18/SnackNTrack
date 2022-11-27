9faf4b0869dcf4dcd16d66138a3280433eb3afe7
SCWinter259 <catungthongkhi2002@gail.com>
2022-11-27 01:00:13 -0700

    added colors and icons, ongoing view recipe

---
+++ app/src/main/java/com/cmput301f22t18/snackntrack/NonScrollableListView.java
@@ -0,0 +1,29 @@
+package com.cmput301f22t18.snackntrack;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+public class NonScrollableListView extends ListView {
+
+    public NonScrollableListView(Context context) {
+        super(context);
+    }
+
+    public NonScrollableListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NonScrollableListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int heightMeasureSpec_custom = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec_custom);
+        ViewGroup.LayoutParams params = getLayoutParams();
+        params.height = getMeasuredHeight();
+    }
+}
