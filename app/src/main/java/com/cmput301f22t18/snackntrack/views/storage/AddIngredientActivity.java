9cafd780fab7fe39f4f71ecc9801f8025c642b53
Duc Ho <hotienduc05012002@gmail.com>
2022-11-28 05:18:07 -0700

    Added amount button functionality

--- app/src/main/java/com/cmput301f22t18/snackntrack/views/storage/AddIngredientActivity.java
+++ app/src/main/java/com/cmput301f22t18/snackntrack/views/storage/AddIngredientActivity.java
@@ -2,19 +2,85 @@ package com.cmput301f22t18.snackntrack.views.storage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageButton;
 
 import com.cmput301f22t18.snackntrack.R;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Locale;
 
 public class AddIngredientActivity extends AppCompatActivity {
-    ImageButton backButton;
+    ImageButton backButton, increaseAmountButton, decreaseAmountButton, calendarButton;
+    TextInputEditText descriptionEditText, amountEditText, bbfEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_ingredient);
         backButton = findViewById(R.id.add_ingredient_back_button);
+        increaseAmountButton = findViewById(R.id.add_an_ingredient_increase_amount_button);
+        decreaseAmountButton = findViewById(R.id.add_an_ingredient_decrease_amount_button);
+        calendarButton = findViewById(R.id.add_an_ingredient_calendar_button);
+
+        descriptionEditText = findViewById(R.id.add_an_ingredient_description_edit_text);
+        amountEditText = findViewById(R.id.add_an_ingredient_amount_edit_text);
+        bbfEditText = findViewById(R.id.add_an_ingredient_bbf_edit_text);
+
+        descriptionEditText.setFocusableInTouchMode(true);
+        amountEditText.setFocusableInTouchMode(true);
+        bbfEditText.setFocusableInTouchMode(false);
+        bbfEditText.setFocusable(false);
+
         backButton.setOnClickListener(v->goBack());
+        increaseAmountButton.setOnClickListener(v->changeAmount(1));
+        decreaseAmountButton.setOnClickListener(v->changeAmount(-1));
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View v = getCurrentFocus();
+
+        if (v != null && (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_MOVE) &&
+                v instanceof TextInputEditText &&
+                !v.getClass().getName().startsWith("android.webkit.")) {
+            int[] sourceCoordinates = new int[2];
+            v.getLocationOnScreen(sourceCoordinates);
+            float x = ev.getRawX() + v.getLeft() - sourceCoordinates[0];
+            float y = ev.getRawY() + v.getTop() - sourceCoordinates[1];
+
+            if (x < v.getLeft() || x > v.getRight() || y < v.getTop() || y > v.getBottom()) {
+                hideKeyboard(this);
+                v.clearFocus();
+            }
+
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    private void hideKeyboard(AddIngredientActivity activity) {
+        if (activity != null && activity.getWindow() != null) {
+            activity.getWindow().getDecorView();
+            InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (imm != null) {
+                imm.hideSoftInputFromWindow(activity.getWindow().getDecorView().getWindowToken(), 0);
+
+            }
+        }
+    }
+
+    public void changeAmount(int changeAmount){
+        int amount = 0;
+        if (amountEditText.getText() != null && !amountEditText.getText().toString().isEmpty()) {
+            amount = Integer.parseInt(amountEditText.getText().toString());
+        }
+        amount += changeAmount;
+        String new_amount = String.format(Locale.CANADA, "%d", amount);
+        amountEditText.setText(new_amount);
     }
 
     public void goBack() {
