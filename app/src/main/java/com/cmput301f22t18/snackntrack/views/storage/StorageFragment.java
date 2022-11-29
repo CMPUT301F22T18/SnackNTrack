ae7b899d385888917bfd766baeb53c64b857f733
Duc Ho <hotienduc05012002@gmail.com>
2022-11-29 13:27:39 -0700

    Added Sorting UI functionality

--- app/src/main/java/com/cmput301f22t18/snackntrack/views/storage/StorageFragment.java
+++ app/src/main/java/com/cmput301f22t18/snackntrack/views/storage/StorageFragment.java
@@ -12,10 +12,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 import com.cmput301f22t18.snackntrack.R;
+import com.cmput301f22t18.snackntrack.RecipeListFragment;
 import com.cmput301f22t18.snackntrack.controllers.StorageAdapter;
 import com.cmput301f22t18.snackntrack.models.Ingredient;
 import com.cmput301f22t18.snackntrack.models.Storage;
@@ -24,6 +28,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
@@ -31,11 +36,12 @@ import java.util.ArrayList;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class StorageFragment extends Fragment {
+public class StorageFragment extends Fragment implements PopupMenu.OnMenuItemClickListener {
 
     Storage storage;
     RecyclerView recyclerView;
     StorageAdapter storageAdapter;
+    ImageButton sortButton;
     ArrayList<String> ingredientIDs;
     FloatingActionButton fab;
     String ERROR_TAG = "STORAGE ERROR";
@@ -86,20 +92,64 @@ public class StorageFragment extends Fragment {
             intent.setClass(getContext(), AddIngredientActivity.class);
             startActivity(intent);
         });
-        setUpStorage();
-
+        setUpStorage("default");
+        sortButton = v.findViewById(R.id.storage_sort_button);
+        sortButton.setOnClickListener(this::displaySortingOptions);
         return v;
     }
 
-    public void setUpStorage() {
+    private void displaySortingOptions(View v) {
+        PopupMenu popup = new PopupMenu(getContext(), v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.storage_sort_menu);
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.storage_sort_description) {
+            //Toast.makeText(this.getContext(), "Title", Toast.LENGTH_SHORT).show();
+            setUpStorage("description");
+            return true;
+        }
+        else if(id == R.id.storage_sort_date) {
+            //Toast.makeText(this.getContext(), "Time", Toast.LENGTH_SHORT).show();
+            setUpStorage("bestBeforeDate");
+            return true;
+        }
+        else if(id == R.id.storage_sort_category) {
+            //Toast.makeText(this.getContext(), "Servings", Toast.LENGTH_SHORT).show();
+            setUpStorage("category");
+            return true;
+        }
+        else if(id == R.id.storage_sort_location) {
+            //Toast.makeText(this.getContext(), "Category", Toast.LENGTH_SHORT).show();
+            setUpStorage("location");
+            return true;
+        }
+        else if(id == R.id.storage_sort_default) {
+            //Toast.makeText(this.getContext(), "Default", Toast.LENGTH_SHORT).show();
+            setUpStorage("default");
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void setUpStorage(String option) {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             storage.clearStorage();
             ingredientIDs = new ArrayList<>();
             String uid = user.getUid();
             FirebaseFirestore db = FirebaseFirestore.getInstance();
-            CollectionReference cr = db.collection("storages")
+            Query cr = db.collection("storages")
                     .document(uid).collection("ingredients");
+            if (!option.equals("default")) {
+                cr = cr.orderBy(option);
+            }
             cr.addSnapshotListener((value, error) -> {
                 if (error != null) {
                     Log.d(ERROR_TAG, error.getLocalizedMessage());
